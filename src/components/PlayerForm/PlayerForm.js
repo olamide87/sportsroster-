af6dd49d6d52684a904f27a94b6b45d778022d2b
Olamide Oladinni <olajide1987@gmail.com>
2020-08-28 00:57:28 -0500

    wip

---
+++ src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PlayerForm extends React.Component {
+  render() {
+    return (
+      <div className="Playerorm">Here is A Player Form</div>
+    );
+  }
+}
+
+export default PlayerForm;
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import playerData from '../../helpers/data/playerData';
 import Player from '../Player/Player';
+import PlayerForm from '../PlayerForm/PlayerForm';
+
 import './Team.scss';
 
 class Team extends React.Component {
@@ -11,6 +14,7 @@ class Team extends React.Component {
 
   state = {
     players: [],
+    formOpen: false,
   }
 
   updatePlayers = () => {
@@ -32,8 +36,17 @@ class Team extends React.Component {
       .catch((err) => console.error('Delete player failed', err));
   }
 
+  createPlayer = (newPlayer) => {
+    playerData.createPlayer(newPlayer)
+      .then(() => {
+        this.getPlayers();
+        this.setState({ formOpen: false });
+      })
+      .catch((err) => console.error('Create Player Broke', err));
+  }
+
   render() {
-    const { players } = this.state;
+    const { players, formOpen } = this.state;
     const playerCard = players.map((player) => <Player key={player.id} player={player} deletePlayer={this.deletePlayer} />);
 
     return (
